43500134bcb3a689011aa22a5b90f97e0ea44896
AndrejDev <andrejpanev8@gmail.com>
2024-08-17 22:38:20 +0200

    user login done with their personal pin

--- Coffee-bar-management-system/Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Coffee-bar-management-system/Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,99 +1,90 @@
-using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Authentication;
 using Entity.Models.Identity;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
+using Microsoft.EntityFrameworkCore;
 
-namespace EShop.Web.Areas.Identity.Pages.Account
+public class LoginModel : PageModel
 {
-    public class LoginModel : PageModel
+    private readonly SignInManager<CbmsUser> _signInManager;
+    private readonly UserManager<CbmsUser> _userManager;
+    private readonly ILogger<LoginModel> _logger;
+
+    public LoginModel(SignInManager<CbmsUser> signInManager, UserManager<CbmsUser> userManager, ILogger<LoginModel> logger)
     {
-        private readonly SignInManager<CbmsUser> _signInManager;
-        private readonly UserManager<CbmsUser> _userManager;
-        private readonly ILogger<LoginModel> _logger;
-
-        public LoginModel(SignInManager<CbmsUser> signInManager, UserManager<CbmsUser> userManager, ILogger<LoginModel> logger)
-        {
-            _signInManager = signInManager;
-            _userManager = userManager;
-            _logger = logger;
-        }
-
-        [BindProperty]
-        public InputModel Input { get; set; }
-
-        public IList<AuthenticationScheme> ExternalLogins { get; set; }
-
-        public string ReturnUrl { get; set; }
-
-        [TempData]
-        public string ErrorMessage { get; set; }
-
-        public class InputModel
-        {
-            [Required]
-            [EmailAddress]
-            public string Email { get; set; }
-
-            [Required]
-            [DataType(DataType.Password)]
-            public string Password { get; set; }
-
-            [Display(Name = "Remember me?")]
-            public bool RememberMe { get; set; }
-        }
-
-        public async Task OnGetAsync(string returnUrl = null)
-        {
-            if (!string.IsNullOrEmpty(ErrorMessage))
-            {
-                ModelState.AddModelError(string.Empty, ErrorMessage);
-            }
-
-            returnUrl ??= Url.Content("~/");
-
-            // Clear the existing external cookie to ensure a clean login process
-            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
-
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-
-            ReturnUrl = returnUrl;
-        }
-
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-        {
-            returnUrl ??= Url.Content("~/");
-
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-
-            if (ModelState.IsValid)
-            {
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
-
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
-                }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToPage("./Lockout");
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return Page();
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return Page();
-        }
+        _signInManager = signInManager;
+        _userManager = userManager;
+        _logger = logger;
     }
+
+    [BindProperty]
+    public InputModel Input { get; set; }
+
+    public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+    public string ReturnUrl { get; set; }
+
+    [TempData]
+    public string ErrorMessage { get; set; }
+
+    public class InputModel
+    {
+        [Required]
+        public int PersonalPin { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public async Task OnGetAsync(string returnUrl = null)
+    {
+        if (!string.IsNullOrEmpty(ErrorMessage))
+        {
+            ModelState.AddModelError(string.Empty, ErrorMessage);
+        }
+
+        returnUrl ??= Url.Content("~/");
+
+        // Clear the existing external cookie to ensure a clean login process
+        await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+        ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+        ReturnUrl = returnUrl;
+    }
+
+    public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+    {
+        returnUrl ??= Url.Content("~/");
+
+        ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+        if (ModelState.IsValid)
+        {
+            // Find the user by Personal PIN
+            var user = await _userManager.Users
+                .SingleOrDefaultAsync(u => u.PersonalPin == Input.PersonalPin);
+
+            if (user != null)
+            {
+                // Sign in the user
+                await _signInManager.SignInAsync(user, Input.RememberMe);
+                _logger.LogInformation("User logged in.");
+
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid PIN.");
+                return Page();
+            }
+        }
+
+        // If we got this far, something failed, redisplay form
+        return Page();
+    }
+
 }
