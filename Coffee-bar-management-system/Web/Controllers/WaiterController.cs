0d8fa5193fc4f25308d6073667e34add4a23c2b0
MCA\dip <dip@mca.dev>
2024-09-15 13:08:03 +0200

    changed error handling in order again

--- Coffee-bar-management-system/Web/Controllers/WaiterController.cs
+++ Coffee-bar-management-system/Web/Controllers/WaiterController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using Repository;
 using Service;
 using System.Security.Claims;
@@ -195,36 +194,56 @@ public class WaiterController : Controller
             return NotFound();
         }
         
-        if (ModelState.IsValid)
+        if (!ModelState.IsValid)
         {
-            for (int i = 0; i < productIds.Count; i++)
+            return RedirectToAction(nameof(Index));
+        }
+
+        bool hasError = false;
+        List<string> errorMessages = [];
+        List<Tuple<Product, int>> productsToAdd = new List<Tuple<Product, int>>();
+
+        for (int i = 0; i < productIds.Count; i++)
+        {
+            Product? product = _productsService.GetProductDetails(productIds[i]);
+            if (product == null)
             {
-                Product? product = _productsService.GetProductDetails(productIds[i]);
-                
-                if (product != null)
-                {
-                    var availableQuantity = product.Quantity;
-                    var seekedQuantity = quantities[i];
-                    if (quantities[i] < 0)
-                    {
-                        ViewData["errorMessage"] = "Quantity cannot be a negative number.";
-                        return View("OrderAgain", order);
-                    }
-                    if (seekedQuantity <= availableQuantity)
-                    {
-                        _waiterService.AddProductToOrder(order, product, seekedQuantity);
-                    }
-                    else
-                    {
-                        return View("NotEnoughProducts", product);
-                    }
-                }
+                break;
+            }
+
+            var availableQuantity = product.Quantity;
+            var seekedQuantity = quantities[i];
+
+            if (seekedQuantity > availableQuantity)
+            {
+                errorMessages.Add($"Not enough {product.Name} in storage. You selected {seekedQuantity}, there is only {availableQuantity} in storage.");
+                hasError = true;
+            }
+            else
+            {
+                productsToAdd.Add(new Tuple<Product, int>(product, seekedQuantity));
+            }
+        }
+
+        if (hasError)
+        {
+            var products = _productsService.GetAllProducts();
+            ViewData["Products"] = new SelectList(products, "Id", "Name");
+            ViewData["Order"] = order;
+            ViewData["ErrorMessages"] = errorMessages;
+
+            return View("OrderAgain");
+        }
+        else
+        {
+            foreach (var product in productsToAdd)
+            {
+                _waiterService.AddProductToOrder(order, product.Item1, product.Item2);
             }
 
             _waiterService.ChangeOrderState(order, State.NEW);
-            return RedirectToAction(nameof(Index));
         }
-        
+
         return RedirectToAction(nameof(Index));
     }
 }
